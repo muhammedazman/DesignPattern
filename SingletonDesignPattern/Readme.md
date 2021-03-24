91f914092c50ca3133c015c4bf844e985da8af6c
DESKTOP-J343IEJ\akyus <vmazman@gmail.com>
2021-03-24 21:12:45 +0100

    Project initialized

---
+++ SingletonDesignPattern/Readme.md
@@ -0,0 +1,7 @@
+##SINGLETON DESIGN PATTERN##
+
+- Eager Initialization
+- Static Block initialization
+- Lazy initialization
+- ThreadSafe Singleton
+- Bill Pugb Singleton
---
+++ SingletonDesignPattern/src/com/muhammedazman/Main.java
@@ -0,0 +1,16 @@
+package com.muhammedazman;
+
+public class Main {
+
+    /***
+     * SINGLETON DESIGN PATTERN
+     * - Eager Initialization
+     * - Static Block initialization
+     * - Lazy initialization
+     * - ThreadSafe Singleton
+     * - Bill Pugb Singleton
+     */
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
